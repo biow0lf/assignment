ccddd70a659581f8ead69314deae3694a4ebda62
Igor Zubkov <biow0lf@localhost.localdomain>
2019-12-10 15:34:03 +0200

    Test data

--- test_data.rb
+++ test_data.rb
@@ -8,3 +8,13 @@ CSV.open("file.csv", "wb") do |csv|
     csv << [(Time.zone.now + t.minutes).iso8601, "#{ Faker::Quote.matz }"]
   end
 end
+
+array = []
+
+10000.times do |t|
+  array.append(time: (Time.zone.now + t.minutes).iso8601, text: "#{ Faker::Quote.matz }")
+end
+
+File.open('file.json', 'w') do |f|
+  f << array.to_json
+end
